ad1fcb63a74b5331e2a92c5f784762fb8916959e
thisistheaj <ajbeckner@gmail.com>
2017-09-29 11:16:44 -0400

    added cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { HttpModule } from '@angular/http';
+import { SwingModule } from 'angular2-swing';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -21,6 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    SwingModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
